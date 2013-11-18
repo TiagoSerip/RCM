a61adaa3ddc1745cde881104cd3966e21220c3d9
Ivo Pereira <ivofpereira@gmail.com>
2013-11-18 20:03:45 +0000

    agent e sonet

--- RCM/sonetGWT/src/main/java/pt/ist/sonet/domain/Agent.java
+++ RCM/sonetGWT/src/main/java/pt/ist/sonet/domain/Agent.java
@@ -44,7 +44,9 @@ public class Agent extends Agent_Base implements Serializable{
 	 * @param Publication pub - publicacao que se quer comentar
 	 * @param String comment - texto do comentario
 	 */
-	public void commentAP(AP ap, String comment) throws YouArentAFriendException {
+	public void commentAP(Comment comment) {
+		
+		this.addComments(comment);
 //		if(pub.getAgent().getPermission().canAcess(this, pub.getAgent())) {
 //			Comment comentario = new Comment(this, pub, pub.generateCommentId(), comment);
 //			pub.commentPublication(comentario);
@@ -55,38 +57,6 @@ public class Agent extends Agent_Base implements Serializable{
 	}
 
 
-	/**
-	 * Metodo que adiciona um voto positivo a uma publicacao. Verifica primeiro se pode votar e,
-	 * se sim, adiciona o voto chamando para isso o metodo positiveVote da publicacao
-	 * @param voteLimit 
-	 * 
-	 * @param Publication pub - publicacao na qual se quer votar
-	 * @throws YouArentAFriendException
-	 */
-	public void positiveVote(Publication pub, int voteLimit) throws YouArentAFriendException {
-		
-//		if(this.canVote(pub)&& !pub.checkVoteLimit(voteLimit)) {
-//			pub.positiveVote(this);
-//		}
-//		else throw new YouArentAFriendException(pub.getAgent().getName());
-
-	}
-
-	/**
-	 * Metodo que adiciona um voto negativo a uma publicacao. Verifica primeiro se pode votar e,
-	 * se sim, adiciona o voto chamando para isso o metodo negativeVote da publicacao
-	 * 
-	 * @param Publication pub - publicacao na qual se quer votar
-	 * @throws YouArentAFriendException
-	 */
-	public void negativeVote(AP ap) throws YouArentAFriendException {
-
-//		if(this.canVote(pub) && !pub.checkVoteLimit(voteLimit)) {
-//			pub.negativeVote(this);
-//		}
-//		else throw new YouArentAFriendException(pub.getAgent().getName());
-	}
-
 
 	/**
 	 * Metodo que retorna informacoes do agente (nome, cidade, etc)
@@ -94,7 +64,7 @@ public class Agent extends Agent_Base implements Serializable{
 	 * @return String
 	 */
 	public String toString() {
-		return this.getType() + " Agent | " + this.getUsername() + " | " + this.getName() + " | " + this.getPassword() + " | " + this.getAp() + ", " + this.getRssi()+", " + this.getIp();
+		return "Agent | " + this.getUsername() + " | " + this.getName() + " | " + this.getPassword() + " | " + this.getAp() + ", " + this.getRssi()+", " + this.getIp();
 	}
 
 	
--- RCM/sonetGWT/src/main/java/pt/ist/sonet/domain/SoNet.java
+++ RCM/sonetGWT/src/main/java/pt/ist/sonet/domain/SoNet.java
@@ -114,6 +114,78 @@ public class SoNet extends SoNet_Base implements Serializable {
 		}
 	}
 	
+	/**
+	 * Obtem um agente individual pelo username
+	 * 
+	 * @param user
+	 * @return Individual ou null se nao existir
+	 */
+	public AP getApBySubnet(String subnet) {
+		for(AP a : getApSet()) {
+			if(a.getSubnet().equalsIgnoreCase(subnet)) {
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Verifica se existe um agent com um dado username
+	 * 
+	 * @param name
+	 * @return boolean com o resultado
+	 */
+	public boolean hasApById(int id) {
+		return getApById(id) != null;
+	}
+
+
+	/**
+	 * Cria um novo agente individual na rede.
+	 * Verifica a validade dos dados de utilizador de acordo com
+	 * as regras de negocio estabelecidas. Lanca excepcoes caso
+	 * existam dados invalidos.
+	 * 
+	 * @param username
+	 * @param name
+	 * @param email
+	 * @param password
+	 * @param city
+	 * @param country
+	 * @return Individual instacia de agente Individual criada.
+	 * @throws UsernameAlreadyExistsException
+	 */
+	public AP createAP (int id, String subnet)
+					throws UsernameAlreadyExistsException {
+		
+		if(this.hasApById(id))
+			throw new UsernameAlreadyExistsException(""+id);
+
+		AP ap = new AP();
+		ap.init(id, subnet, 0, 0);
+		return ap;
+	}
+
+	@Override
+	public void addAp(AP ap) {
+		super.addAp(ap);
+	}
+	
+	/**
+	 * Remove um agente da base de dados da SoNet.
+	 * Lanca excepcao em caso de username inexistente.
+	 * 
+	 * @param username
+	 * @throws AgentUsernameDoesNotExistsException
+	 */
+	public void removeAP(int id)
+			throws AgentUsernameDoesNotExistsException {
+		AP toRemove = this.getApById(id);
+		if (toRemove == null)
+			throw new AgentUsernameDoesNotExistsException(""+id);
+		super.removeAp(toRemove);
+	}
+	
 
 	/**
 	 * Obtem da BD a publicacao com o id indicado.
@@ -141,21 +213,17 @@ public class SoNet extends SoNet_Base implements Serializable {
 	 * @return Publication
 	 * @throws AgentUsernameDoesNotExistsException
 	 */
-	public Publication getUserLatestComment(String username) throws AgentUsernameDoesNotExistsException {
-		Agent agent = this.getAgentByUsername(username);
-		if (agent == null) {
-			throw new AgentUsernameDoesNotExistsException(username);
+	public Comment getApLatestComment(int id) throws AgentUsernameDoesNotExistsException {
+		AP ap = this.getApById(id);
+		if (ap == null) {
+			throw new AgentUsernameDoesNotExistsException(""+id);
 		}
-		if(agent.getCommentsSet().isEmpty()) {
+		if(ap.getCommentsSet().isEmpty()) {
 			return null;
 		}
-		int id=0;
-		for(Publication p : agent.getPublicationsSet()) {
-			if(p.getId() > id) {
-				id = p.getId();
-			}
-		}
-		return this.getPublicationById(id);
+		
+		return ap.getCommentsSet().iterator().next();
+
 	}
 
 	/**
@@ -166,105 +234,12 @@ public class SoNet extends SoNet_Base implements Serializable {
 	 * @return Set<Publication> Set com as publicacoes do agente.
 	 * @throws AgentUsernameDoesNotExistsException
 	 */
-	public Set<Publication> getUserPublicationsByUsername(String username)
+	public Set<Comment> getApComments(int id)
 			throws AgentUsernameDoesNotExistsException {
-		Agent agent = this.getAgentByUsername(username);
-		if (agent == null)
-			throw new AgentUsernameDoesNotExistsException(username);
-		return agent.getPublicationsSet();
-	}
-
-	/**
-	 * Cria uma nova publicacao do tipo nota.
-	 * 
-	 * @param label
-	 * @param creator
-	 * @param text
-	 * @return Note instacia de nota criada.
-	 */
-	public Note createNote(String label, Agent creator, String text) {
-		int id = this.getPublicationIds();
-
-		Note note = new Note(id, label, 0, 0, creator, text);
-		id = id + 1;
-		this.setPublicationIds(id);
-		return note;
-	}
-
-	/**
-	 * Cria uma nova publicacao do tipo conteudo.
-	 * @param label
-	 * @param creator
-	 * @param url
-	 * @return Content instacia de conteudo cirado.
-	 */
-	public Content createContent(String label, Agent creator, String url, int price) {
-		int id = this.getPublicationIds();
-
-		Content content = new Content(id, label, 0, 0, creator, url, price);
-		id = id + 1;
-		this.setPublicationIds(id);
-		
-		return content;
-	}
-
-	/**
-	 * Adiciona uma nota 'a BD.
-	 * @param note
-	 */
-	public void addNote(Note note) {
-		this.addPublications(note);
-	}
-
-	/**
-	 * Adiciona um conteudo 'a BD.
-	 * @param content
-	 */
-	public void addContent(Content content) {
-		this.addPublications(content);
-	}
-
-	/**
-	 * Obtem um Set de comentarios de uma determianda publicaco por id
-	 * de publicacao.
-	 * @param id
-	 * @return
-	 */
-	public Set<Comment> getPublicationCommentsByPublicationId(int id) {
-		Publication pub = this.getPublicationById(id);
-		Set<Comment> comments = pub.getCommentsSet();
-		return comments;
-	}
-
-	/**
-	 * Efectua um pedido de ligacao entre dois agentes.
-	 * @param from
-	 * @param to
-	 */
-	public void friendRequest(Individual from, Agent to) {
-		if(!from.checkFriendLimit(this.getFriendLimit(), this.getAgentFriendNumber(from)) && !to.checkFriendLimit(this.getFriendLimit(), this.getAgentFriendNumber(to))){
-			from.sendFriendRequest(to);
-		}
-	}
-
-	/**
-	 * Confirma um pedido de ligacao entre dois agentes do tipo Individual.
-	 * @param from
-	 * @param to
-	 */
-	public void confirmFriendRequest(Individual from, Individual to) {
-		if(!from.checkFriendLimit(this.getFriendLimit(), this.getAgentFriendNumber(from)) && !to.checkFriendLimit(this.getFriendLimit(), this.getAgentFriendNumber(to))){
-			to.acceptRequest(from);
-		}
-	}
-
-	/**
-	 * Recusa um pedido de ligacao entre dois agentes do tipo Individual.
-	 * @param from
-	 * @param to
-	 */
-	public void refuseFriendRequest(Individual from, Individual to) {
-		to.refuseRequest(from);
+		AP ap = this.getApById(id);
+		if (ap == null)
+			throw new AgentUsernameDoesNotExistsException(""+id);
+		return ap.getCommentsSet();
 	}
 
 	/**
@@ -273,8 +248,8 @@ public class SoNet extends SoNet_Base implements Serializable {
 	 * @param Agent voter - agente que vota na publicacao
 	 * @param Publication pub - publicacao na qual se vai votar
 	 */
-	public void posVote(Agent voter, Publication pub) {
-		voter.positiveVote(pub, this.getVoteLimit());		
+	public void posVote(Agent voter, AP ap) {
+		ap.positiveVote(voter);
 	}
 
 	/**
@@ -283,9 +258,9 @@ public class SoNet extends SoNet_Base implements Serializable {
 	 * @param Agent voter - agente que vota na publicacao
 	 * @param Publication pub - publicacao na qual se vai votar
 	 */
-	public void negVote(Agent voter, Publication pub) {
-		voter.negativeVote(pub, this.getVoteLimit());
-			}
+	public void negVote(Agent voter, AP ap) {
+		ap.positiveVote(voter);
+	}
 
 	/**
 	 * Comenta, se possivel, uma publicacao
@@ -294,40 +269,11 @@ public class SoNet extends SoNet_Base implements Serializable {
 	 * @param Publication publicacao
 	 * @param String comentario
 	 */	
-	public void commentPublication(Agent commentator, Publication pub, String comment) throws YouArentAFriendException {
-		if(pub.getAgent().getPermission().canAcess(commentator, pub.getAgent()) && !pub.checkVoteLimit(this.getVoteLimit())) {
-			Comment comentario = new Comment(commentator, pub, pub.generateCommentId(), comment);
-			pub.commentPublication(comentario);
-		}
-		else { //por enquanto, o unico motivo que o leva a nao ter permissao e nao ser amigo
-			throw new YouArentAFriendException(pub.getAgent().getName());
-		}
+	public void commentAp(Agent agent, AP ap, String comment) {
+		Comment comentario = new Comment(agent, ap, comment);
+		ap.commentAp(comentario);	
+		agent.commentAP(comentario);
 	}
-		
-	/**
-	 * Verifica se um agente Individual ja excedeu o limite de amigos
-	 * 
-	 * @param Individual agent - agente a verificar
-	 */
-	public int getAgentFriendNumber(Agent agent) {
-		int friends=0;		
-		friends=(agent.getAgentCount()+agent.getFriendsCount());
-		return friends;
-	}
-	
-	/**
-	 * Lista todos os amigos de um dado agent
-	 * 
-	 * @param Individual agent - agente a verificar
-	 * @return ArrayList<Agent> - Lista de Agents que sao amigo do agent
-	 */
-	public ArrayList<Agent> getFriendsOf(Agent agent) {
-		ArrayList<Agent> list = new ArrayList<Agent>();
-		
-		list.addAll(agent.getAgentSet());
-		list.addAll(agent.getFriendsSet());
 
-		return list;
-	}
 
 }
