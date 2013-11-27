75e004478dce9f1638f9dd97684f93a9e3d97a95
TiagoSerip <tiagopires107@gmail.com>
2013-11-27 18:05:00 +0000

    adicionado serviço removepiservice

---
+++ RCM/sonetGWT/src/main/java/pt/ist/sonet/service/RemovePIService.java
@@ -0,0 +1,40 @@
+package pt.ist.sonet.service;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.sonet.domain.AP;
+import pt.ist.sonet.domain.PI;
+import pt.ist.sonet.domain.SoNet;
+import pt.ist.sonet.exception.AgentUsernameDoesNotExistsException;
+import pt.ist.sonet.exception.ApIdDoesNotExistsException;
+import pt.ist.sonet.exception.SoNetException;
+import pt.ist.sonet.service.dto.*;
+
+public class RemovePIService extends SonetService {
+
+	private PIDto piDto;
+	private int apID;
+	
+	public RemovePIService(int ap, PIDto pi) {
+		this.piDto = pi;
+		this.apID = ap;
+	}
+	
+	/**
+	 * 
+	 * Faz o envio (dispatch) do servico 
+	 * 
+	 * @throws SoNetException
+	 * @throws AgentUsernameDoesNotExistsException
+	 * @throws YouArentAFriendException
+	 */
+	@Override
+	protected void dispatch() throws SoNetException{
+		
+		SoNet sonet = FenixFramework.getRoot();
+		AP ap = sonet.getApById(apID);
+		if(ap == null)
+			throw new ApIdDoesNotExistsException(apID);
+		PI pi = sonet.getPIByID(piDto.getId());
+		sonet.removePI(pi, ap);
+	}
+}
